920f0dec0f5dd81d94436811f261c2cd59470fa1
jupemara <jumpei.arashi@arashike.com>
2021-06-06 15:43:52 +0900

    list handler

--- main.go
+++ main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	handler "github.com/jupemara/appengine-demo/adapter/controller/http/user"
+	"github.com/jupemara/appengine-demo/adapter/repository/user/csv"
+	domain "github.com/jupemara/appengine-demo/domain/model/user"
+	usecase "github.com/jupemara/appengine-demo/usecase/user"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
+	mux := http.NewServeMux()
+	listHandler := handler.NewHttpUserListHandler(
+		usecase.NewListUserUsecase(
+			[]domain.UserRepository{
+				csv.NewUserRepository("./data/users.csv"),
+			},
+		),
+	)
+	mux.Handle("/list", listHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
-
 	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprint(w, "Hello, World!")
 }
